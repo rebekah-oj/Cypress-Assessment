6c3740fe246c5e4da487495550333035fcb579ce
rebekahoj <olojadeborah37@gmail.com>
2024-06-28 09:51:05 +0100

    login page tests

--- cypress/e2e/LoginTests.cy.js
+++ cypress/e2e/LoginTests.cy.js
@@ -1,5 +1,43 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import {LoginPage} from '../e2e/LoginPage.cy'; 
+
+describe('Login Page Tests', () => {
+
+    const loginPage = new LoginPage();
+
+    beforeEach(() => {
+       cy.visit('https://the-internet.herokuapp.com/login'); // visit url
+    });
+
+    it('should login successfully with valid credentials', () => {
+
+        loginPage.enterUsername('tomsmith');
+        loginPage.enterPassword('SuperSecretPassword!');
+        loginPage.clickLoginButton();
+
+        cy.wait(1000)
+        loginPage.getSuccessMessage().should('have.text', '\n            You logged into a secure area!\n            ×\n          '); // verify success message contains this text
+
+    });
+
+    it('should fail to login with invalid credentials', () => {
+
+        loginPage.enterUsername('tomsmith');
+        loginPage.enterPassword('SuperSecretPassword');
+        loginPage.clickLoginButton();
+
+       loginPage.getErrorMessage().should('have.text', '\n            Your password is invalid!\n            ×\n          '); // Verify error message contains this text
+
+    });
+
+    it('should display error message when login fails', () => {
+
+        loginPage.enterUsername('tom');
+        loginPage.enterPassword('SuperSecretPassword');
+        loginPage.clickLoginButton();
+  
+    
+        loginPage.getErrorMessage().should('have.text', '\n            Your username is invalid!\n            ×\n          '); // Verify error message contains this text
+
+    });
+
+});
