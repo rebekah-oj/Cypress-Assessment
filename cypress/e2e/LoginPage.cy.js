e9cc9f8b95831e43f7b943d11f21efff9a298ea3
rebekahoj <olojadeborah37@gmail.com>
2024-06-28 09:48:32 +0100

    login page

--- cypress/e2e/LoginPage.cy.js
+++ cypress/e2e/LoginPage.cy.js
@@ -1,5 +1,49 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+export class LoginPage {
+
+  navigate(){
+    cy.visit('https://the-internet.herokuapp.com/login');
+  }
+
+  // Locators
+  getUsernameField() {
+    return cy.get('#username');
+  }
+
+  getPasswordField() {
+    return cy.get('#password');
+  }
+
+  getLoginButton() {
+    return cy.get('.radius');
+  }
+
+  getErrorMessage() {
+    return cy.get('#flash');
+  }
+
+  getSuccessMessage() {
+      return cy.get('#flash').should('be.visible');
+  }
+
+
+  // Methods for Actions
+  enterUsername(username) {
+    this.getUsernameField().type(username);
+  }
+
+  enterPassword(password) {
+    this.getPasswordField().type(password);
+  }
+
+  clickLoginButton() {
+    this.getLoginButton().click();
+  }
+
+  verifyErrorMessage() {
+    this.getErrorMessage().should('be.visible');
+  }
+
+  //verifySuccessMessage() {
+   //   this.getSuccessMessage().should('be.visible');
+   // }
+}
